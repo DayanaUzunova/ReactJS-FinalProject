b0fb109ae64cf1e23e12fbc0182d1c00986cc23b
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-26 22:22:33 +0300

    Created useForm.js file.

---
+++ Courses Management System/client/src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export function useForm(initialValues, submitCallback) {
+    const [values, setValues] = useState(initialValues);
+
+    // TODO: add support for checkbox
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        submitCallback(values);
+    }
+
+    return {
+        values,
+        changeHandler,
+        submitHandler
+    };
+}
