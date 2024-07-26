25c54fb95cdb5e7449134db9da5d484b1ddf62db
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-26 22:23:26 +0300

    Updated my create function.

--- Courses Management System/client/src/components/course-create/CourseCreate.jsx
+++ Courses Management System/client/src/components/course-create/CourseCreate.jsx
@@ -1,24 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm.js";
+import { useCreateCourse } from "../../hooks/useCourses";
+
+const initialValues = {
+    title: '',
+    category: '',
+    imageUrl: '',
+    teacherName: '',
+    description: ''
+}
+
 export default function CourseCreate() {
+    const navigate = useNavigate();
+    const createCourse = useCreateCourse();
+
+    const createHandler = async (values) => {
+        try{
+            const { _id: courseId } = await createCourse(values);
+
+            navigate(`/courses/${courseId}/details`);
+        }
+        catch (err) {
+            //TODO Set error state and display error
+            console.log(err.message);
+        }
+    };
+
+    const { values, changeHandler, submitHandler } = useForm(initialValues, createHandler);
+    
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={submitHandler}>
                 <div className="container">
 
                     <h1>Create Course</h1>
                     <label className="inputs" htmlFor="leg-title">Title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter course name..." />
+                    <input type="text" id="title" name="title" value={values.title} onChange={changeHandler} placeholder="Enter course name..." />
 
                     <label className="inputs" htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" placeholder="Enter course category..." />
+                    <input type="text" id="category" name="category" value={values.category} onChange={changeHandler} placeholder="Enter course category..." />
 
-                    <label className="inputs" htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                    <label className="inputs" htmlFor="course-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" value={values.imageUrl} onChange={changeHandler} placeholder="Upload a photo..." />
 
                     <label className="inputs" htmlFor="teacherName">Teacher Name:</label>
-                    <input type="text" id="name" name="name" value="" />
+                    <input type="text" id="name" name="name" value={values.teacherName} onChange={changeHandler} />
 
                     <label className="inputs" htmlFor="description">Description:</label>
-                    <textarea name="description" id="description"></textarea>
+                    <textarea name="description" value={values.description} onChange={changeHandler} id="description"></textarea>
                     <input className="btn submit" type="submit" value="Create Course" />
                 </div>
             </form>
