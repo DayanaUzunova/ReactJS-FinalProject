ccb326ab9c44bf245076874c8ba3330724561b28
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-26 22:15:54 +0300

    Created auth.api file.

---
+++ Courses Management System/client/src/api/auth-api.js
@@ -0,0 +1,7 @@
+import requester from "./requester.js";
+
+const BASE_URL = 'http://localhost:3030/users';
+
+export const login = (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
+
+export const register = (email, password) => requester.post(`${BASE_URL}/register`, { email, password });
