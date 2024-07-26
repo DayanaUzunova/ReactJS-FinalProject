39d4668f5153f0c591302a991a8f58cdf83e220e
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-26 22:19:20 +0300

    Added accessToken.

--- Courses Management System/client/src/api/requester.js
+++ Courses Management System/client/src/api/requester.js
@@ -1,12 +1,22 @@
 async function requester(method, url, data) {
     const options = {};
 
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+        options.headers = {
+            ...options.headers,
+            ['X-Authorization']: accessToken
+        }
+    }
+
     if (method != 'GET') {
         options.method = method;
     }
 
     if (data) {
         options.headers = {
+            ...options.headers,
             'Content-Type': 'application/json'
         };
 
@@ -14,7 +24,11 @@ async function requester(method, url, data) {
     }
 
     const response = await fetch(url, options);
-    const result = response.json();
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
 
     return result;
 }
