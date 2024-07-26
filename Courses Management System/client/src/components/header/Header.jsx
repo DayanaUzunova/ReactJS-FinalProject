349c5ffe984e5a23e317807ed73433da333f2901
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-26 22:27:30 +0300

    Updated the navigation with Authentication.

--- Courses Management System/client/src/components/header/Header.jsx
+++ Courses Management System/client/src/components/header/Header.jsx
@@ -1,19 +1,29 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext.js';
 
 export default function Header() {
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <header>
             <h1><Link className="home" to="/">Courses Management System</Link></h1>
             <nav>
                 <Link to="/courses">All courses</Link>
+                {isAuthenticated
+                ? (
                 <div id="user">
                     <Link to="/courses/create">Create Course</Link>
                     <Link to="/logout">Logout</Link>
                 </div>
-                <div id="guest">
+                )
+                : (
+                   <div id="guest">
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
-                </div>
+                </div> 
+                )
+                }  
             </nav>
         </header>
     );
