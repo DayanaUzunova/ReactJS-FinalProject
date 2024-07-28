998b4b1d15e937cd2c823a887b02a35cb063c984
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-28 21:41:51 +0300

    Updated CourseListItem.jsx file

--- Courses Management System/client/src/components/course-list/course-list-item/CourseListItem.jsx
+++ Courses Management System/client/src/components/course-list/course-list-item/CourseListItem.jsx
@@ -4,16 +4,14 @@ export default function CourseListItem({
     _id,
     title,
     category,
-    imageUrl,
-    teacherName
+    imageUrl
 }) {
     return (
-        <div className="allGames">
-            <div className="allGames-info">
+        <div className="allCourses">
+            <div className="allCourses-info">
                 <img src={imageUrl} />
                 <h6>{category}</h6>
                 <h2>{title}</h2>
-                <h2>{teacherName}</h2>
                 <Link to={`/courses/${_id}/details`} className="details-button">Details</Link>
             </div>
         </div>
