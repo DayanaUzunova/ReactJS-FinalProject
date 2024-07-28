7814456fecee02980ff132655fdaa1b4b79e75da
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-28 21:36:21 +0300

    Add logout.
    Add logout.

--- Courses Management System/client/src/api/auth-api.js
+++ Courses Management System/client/src/api/auth-api.js
@@ -5,3 +5,5 @@ const BASE_URL = 'http://localhost:3030/users';
 export const login = (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
 
 export const register = (email, password) => requester.post(`${BASE_URL}/register`, { email, password });
+
+export const logout = () => requester.get(`${BASE_URL}/logout`);
