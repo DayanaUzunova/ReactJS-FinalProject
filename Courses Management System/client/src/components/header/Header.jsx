7c34d8b8ae4b1791ca136e77874e5a216b215625
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-28 21:42:42 +0300

    Create usePersistedState hook.

--- Courses Management System/client/src/components/header/Header.jsx
+++ Courses Management System/client/src/components/header/Header.jsx
@@ -1,29 +1,27 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext.js';
+import { useAuthContext } from '../../contexts/AuthContext.jsx';
 
 export default function Header() {
-    const { isAuthenticated } = useContext(AuthContext);
-
+    const { isAuthenticated } = useAuthContext();
     return (
         <header>
             <h1><Link className="home" to="/">Courses Management System</Link></h1>
             <nav>
-                <Link to="/courses">All courses</Link>
+                <Link to="/courses">All Courses</Link>
                 {isAuthenticated
-                ? (
-                <div id="user">
-                    <Link to="/courses/create">Create Course</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                )
-                : (
-                   <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div> 
-                )
-                }  
+                    ? (
+                        <div id="user">
+                            <Link to="/courses/create">Create Course</Link>
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }
             </nav>
         </header>
     );
