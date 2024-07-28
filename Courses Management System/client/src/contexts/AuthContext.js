4d93b8e99ea09d0bcf8ab5019bffa11392949307
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-28 21:44:34 +0300

    Add getAccessToken.

--- Courses Management System/client/src/contexts/AuthContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const AuthContext = createContext({
-    userId: '',
-    email: '',
-    accessToken: '',
-    isAuthenticated: false,
-    changeAuthState: (authState = {}) => null
-});
---
+++ Courses Management System/client/src/contexts/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext } from "react";
+
+import usePersistedState from "../hooks/usePersistedState.js";
+
+export const AuthContext = createContext({
+    userId: '',
+    email: '',
+    accessToken: '',
+    isAuthenticated: false,
+    changeAuthState: (authState = {}) => null,
+    logout: () => null
+});
+
+export function AuthContextProvider(props) {
+    const [authState, setAuthState] = usePersistedState('auth', {});
+
+    const changeAuthState = (state) => {
+        setAuthState(state);
+    };
+
+    const logout = () => {
+        setAuthState(null);
+    };
+
+    const contextData = {
+        userId: authState?._id,
+        email: authState?.email,
+        accessToken: authState?.accessToken,
+        isAuthenticated: !!authState?.email,
+        changeAuthState,
+        logout
+    };
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuthContext() {
+    const authData = useContext(AuthContext);
+
+    return authData;
+}
