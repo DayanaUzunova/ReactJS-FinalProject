300efef790058192053c75cc3d0e94069cf67cfa
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:15:43 +0300

    Added Edit, Footer and Search.

--- Courses Management System/client/src/App.jsx
+++ Courses Management System/client/src/App.jsx
@@ -8,7 +8,7 @@ import Logout from './components/logout/Logout.jsx'
 import CourseList from './components/course-list/CourseList.jsx';
 import CourseCreate from './components/course-create/CourseCreate.jsx'
 import CourseDetails from './components/course-details/CourseDetails.jsx';
-import GameEdit from './components/course-edit/CourseEdit.jsx'
+import CourseEdit from './components/course-edit/CourseEdit.jsx'
 import Footer from './components/footer/Footer.jsx';
 import Search from './components/search/Search.jsx';
 
@@ -27,7 +27,7 @@ function App() {
                         <Route path='/courses' element={<CourseList />} />
                         <Route path='/search' element={<Search />} />
                         <Route path='/courses/:courseId/details' element={<CourseDetails />} />
-                        <Route path='/games/:courseId/edit' element={<GameEdit />} />
+                        <Route path='/games/:courseId/edit' element={<CourseEdit />} />
                         <Route path='/courses/create' element={<CourseCreate />} />
                     </Routes>
                 </main>
