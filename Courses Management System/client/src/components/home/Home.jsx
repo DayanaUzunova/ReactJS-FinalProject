fb16aebe2db770fa874c4cbb02ed972fd1451b26
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:08:14 +0300

    Updated Home.jsx file.

--- Courses Management System/client/src/components/home/Home.jsx
+++ Courses Management System/client/src/components/home/Home.jsx
@@ -1,20 +1,7 @@
-import { useEffect, useState } from "react";
-import coursesAPI from "../../api/courses-api.js";
-import LatestCourse from "./latest-course/LatestCourse.jsx";
-
 export default function Home() {
-    const [latestCourses, setLatestCourses] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            // TODO: modify to fetch only latest games
-            const result = await coursesAPI.getAll();
-
-            setLatestCourses(result.reverse().slice(0, 3));
-        })();
-    }, []);
 
     return (
+        <>
         <section id="welcome-world">
 
             <div className="welcome-message">
@@ -23,14 +10,7 @@ export default function Home() {
                 <h2>We are here for you!</h2>
             </div>
             <img src="./images/books.png" alt="hero" />
-
-            <div id="home-page">
-                <h1>Latest Courses</h1>
-                {latestCourses.length > 0
-                    ? latestCourses.map(course => <LatestCourse key={course._id} {...course} />)
-                    : <p className="no-courses">No courses yet</p>
-                }
-            </div>
         </section>
+    </>
     );
 }
