d665bfccc5f3c0f1ad9401f63b312b68f5a1c50c
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:06:12 +0300

    Changed the position of category and title.

--- Courses Management System/client/src/components/course-list/course-list-item/CourseListItem.jsx
+++ Courses Management System/client/src/components/course-list/course-list-item/CourseListItem.jsx
@@ -10,8 +10,8 @@ export default function CourseListItem({
         <div className="allCourses">
             <div className="allCourses-info">
                 <img src={imageUrl} />
-                <h6>{category}</h6>
                 <h2>{title}</h2>
+                <h6>{category}</h6>
                 <Link to={`/courses/${_id}/details`} className="details-button">Details</Link>
             </div>
         </div>
