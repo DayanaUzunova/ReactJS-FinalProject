66049800a2eeda8c7803d356bacf21419f209878
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:05:39 +0300

    Added some headings.

--- Courses Management System/client/src/components/course-list/CourseList.jsx
+++ Courses Management System/client/src/components/course-list/CourseList.jsx
@@ -5,12 +5,21 @@ export default function CourseList() {
     const [courses] = useGetAllCourses();
 
     return (
-        <section id="catalog-page">
-            <h1>All Courses</h1>
-            {courses.length > 0
-                ? courses.map(course => <CourseListItem key={course._id} {...course} />)
-                : <h3 className="no-articles">No courses yet</h3>
-            }
-        </section>
+        <div className="page-container">
+            <section id="catalog-page">
+                <h1>All Courses</h1>
+                {courses.length > 0 
+                    ? (
+                        <>
+                            <h2>These are all the courses that have been uploaded to our site!</h2>
+                            <h2>We hope at least one of them impresses you!</h2>
+                            {courses.map(course => <CourseListItem key={course._id} {...course} />)}
+                        </>
+                    ) : (
+                        <h3 className="no-articles">No courses yet</h3>
+                    )
+                }
+            </section>
+        </div>
     );
 }
