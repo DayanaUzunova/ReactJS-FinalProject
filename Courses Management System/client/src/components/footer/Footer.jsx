6b56254c3920fa2e4814a7bf6bae81ae369d0aae
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:06:38 +0300

    Created Footer.

---
+++ Courses Management System/client/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>&copy; 2024 Courses Management System. Всички права запазени.</p>
+                <p>Свържете се с нас: info@coursesSystem.com</p>
+            </div>
+        </footer>
+    );
+}
