7d7692fce1303f7f87cd815cc64fbc0dc0affb93
Dayana Uzunova <daqnauzunova@abv.bg>
2024-08-04 19:07:13 +0300

    Added className to the <h1>.

--- Courses Management System/client/src/components/header/Header.jsx
+++ Courses Management System/client/src/components/header/Header.jsx
@@ -5,18 +5,20 @@ export default function Header() {
     const { isAuthenticated } = useAuthContext();
     return (
         <header>
-            <h1><Link className="home" to="/">Courses Management System</Link></h1>
+            <h1 className='header-title'><Link className="home" to="/">Courses Management System</Link></h1>
             <nav>
                 <Link to="/courses">All Courses</Link>
                 {isAuthenticated
                     ? (
                         <div id="user">
                             <Link to="/courses/create">Create Course</Link>
+                            <Link to='/search'>Search</Link>
                             <Link to="/logout">Logout</Link>
                         </div>
                     )
                     : (
                         <div id="guest">
+                            <Link to='/search'>Search</Link>
                             <Link to="/login">Login</Link>
                             <Link to="/register">Register</Link>
                         </div>
